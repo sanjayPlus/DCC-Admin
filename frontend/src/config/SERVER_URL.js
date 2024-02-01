1ba91f8eab3fcc6ece58226949d02153984a89f1
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-01 16:19:05 +0530

    ds

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,6 +1,6 @@
 // const SERVER_URL = "http://localhost:9000/api"
 
-const SERVER_URL = "https://dccbackend.sanjaysanthosh.me/api"
+const SERVER_URL = "http://dccbackend.sanjaysanthosh.me/api"
 
 
 export default SERVER_URL;
