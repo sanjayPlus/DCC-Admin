8ff748d3a6592b3df65abde5ba70cc725d06bc5c
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-07 12:05:05 +0530

    social media aaded

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,7 +1,7 @@
-// const SERVER_URL = "http://localhost:9000/api"
+const SERVER_URL = "http://localhost:5000/api"
 
 
-const SERVER_URL = "https://dccbackend.plusitpark.com/api"
+// const SERVER_URL = "https://dcc-kozhikode.onrender.com/api"
 // const SERVER_URL = "https://intuc1.onrender.com/api"
 
 
