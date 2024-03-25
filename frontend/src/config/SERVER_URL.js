9f49d1f3fffdd70e26335df00a572fd9990639db
danaplusitpark <danaplusitpark@gmail.com>
2024-03-25 15:59:21 +0530

    serverurl

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -2,11 +2,11 @@
 //     SERVER_URL = localStorage.getItem("api")
 // }
 
-// let SERVER_URL = "https://dccbackend.plusitpark.com/api"
+let SERVER_URL = "https://dccbackend.plusitpark.com/api"
 
 // const SERVER_URL = "https://dcc-kozhikode.onrender.com/api"
 // const SERVER_URL = "https://dccbackend.plusitpark.com/api"
 // const SERVER_URL = "https://intuc1.onrender.com/api"
- const SERVER_URL = "http://localhost:3000/api";
+//  const SERVER_URL = "http://localhost:3000/api";
 
 export default SERVER_URL;
