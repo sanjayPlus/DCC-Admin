283d375269f0fe0ada940a1eb223c071e7b72fc9
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-26 16:30:26 +0530

    id card 2 added

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -293,7 +293,7 @@ function SideBar() {
               <span className="link-title">All Daily News</span>
             </Link>
           </li>
-          <li className="nav-item">
+          {/* <li className="nav-item">
             <Link to="/add-swing" className="nav-link">
               <i className="link-icon" data-feather="box" />
               <span className="link-title">Add Swing</span>
@@ -304,7 +304,7 @@ function SideBar() {
               <i className="link-icon" data-feather="box" />
               <span className="link-title">All Swing</span>
             </Link>
-          </li>
+          </li> */}
     
         </ul>
       </div>
